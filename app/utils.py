3e5b1a58feaff9bd54acaa08547fee67cfa90bf5
Kameel Dossal <103609764+Kdossal@users.noreply.github.com>
2025-12-01 09:54:37 -0500

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,22 +1,24 @@
 time_slots = {
-    "Day 1": {
-        "9:00 AM": None,
-        "10:00 AM": None,
-        "11:00 AM": None,
-        "12:00 PM": "LUNCH",
-        "1:00 PM": None,
-        "2:00 PM": None,
-        "3:00 PM": None,
-        "4:00 PM": None,
-    },
-    "Day 2": {
-        "9:00 AM": None,
+    "Monday, February 23rd": {
         "10:00 AM": None,
         "11:00 AM": None,
         "12:00 PM": "LUNCH",
         "1:00 PM": None,
         "2:00 PM": "BREAK",
         "3:00 PM": None,
-        "4:00 PM": None,
+    },
+    "Tuesday, February 24th": {
+        "10:00 AM": None,
+        "11:00 AM": None,
+        "12:00 PM": "LUNCH",
+        "1:00 PM": None,
+        "2:00 PM": "BREAK",
+        "3:00 PM": None,
+    },
+    "Wednesday, February 25th": {
+        "10:00 AM": None,
+        "11:00 AM": None,
+        "12:00 PM": "LUNCH",
+        "1:00 PM": None,
     }
 }
